699d602ee8779e377c9c9f9d3380afa73f8fbd04
mullak99 <mullak99@mullak99.co.uk>
2018-10-12 06:32:29 +0100

    Updated Changelogs

--- FileAES-CLI/Properties/AssemblyInfo.cs
+++ FileAES-CLI/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FileAES-CLI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A tool used to encrypt and decrypt files or folders using FAES.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("mullak99")]
 [assembly: AssemblyProduct("FileAES-CLI")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2018 | mullak99")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
